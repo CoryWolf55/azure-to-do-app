6486893b1c47bee734d66180194129d39c8dc76f
Cory Wolf <corywolf333@gmail.com>
2026-03-28 12:26:57 -0400

    Basic API setup
    Created a basic .NET API that can create, edit, get, and remove items.
    
    Next is the full setup to frontend as well as deployment of API.

---
+++ Task-API/Controllers/TasksController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task_API.Models;
+using System.Linq;
+
+namespace Task_API.Controllers
+{
+
+    [ApiController]
+    [Route("/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        ///<summary>
+        /// Simple task controller that allows you do to basic calls to create, read, update and delete tasks.
+        /// Can easily add more calls later. 
+        /// 
+        /// To-Do: (some will not be added due to the simplicity of the project, but are good to have in a real application)
+        /// Add more functionality such as filtering tasks by completion status, due date, etc.
+        /// Need to add DTOs and validation for better data handling and error management.
+        /// Rate Limiting and Authentication should be added for better security and performance.
+        /// 
+        ///</summary>
+        
+
+        //Just using in-memory list for simplicity
+        private static List<TaskItem> tasks = new List<TaskItem>();
+
+        [HttpGet]
+        public ActionResult<List<TaskItem>> GetAll()
+        {
+            return Ok(tasks);
+        }
+
+        [HttpPost]
+        public ActionResult<TaskItem> Create(TaskItem task)
+        {
+            task.Id = tasks.Count + 1; // Simple ID generation since im not using a database
+            tasks.Add(task);
+            return Ok();
+        }
+
+        [HttpPut("{id}")] // Update task by ID, Only using ID in route for simplicity
+        public IActionResult Update(int id, TaskItem updatedTask)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            task.IsComplete = updatedTask.IsComplete;
+            return Ok();
+        }
+
+        [HttpDelete("{id}")] // Delete task by ID, Only using ID in route for simplicity
+        public IActionResult Delete(int id)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            tasks.Remove(task);
+            return Ok();
+        }
+    }
+}
---
+++ Task-API/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+#region CORS Configuration
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+#endregion
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
